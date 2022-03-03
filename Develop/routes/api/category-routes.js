8a21e1fb8abdcd2399dd0178d00046518c81c7fb
ramriddlez <ramankangz@outlook.com>
2022-03-03 18:04:16 -0500

    category model added

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,11 +6,17 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((catData) => {
+    res.json(catData);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((catData) =>{
+    res.json(catData)
+  })
 });
 
 router.post('/', (req, res) => {
