eac1989b84d99024478bb8db286fb312cfab1e1d
ramriddlez <ramankangz@outlook.com>
2022-03-03 18:55:47 -0500

    final changes in model and connections

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -15,18 +15,16 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }, foreignKey:'product_id' 
+  through: ProductTag,
+  as: 'tags',
+  foreignKey:'product_id' ,
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }, foreignKey:'tag_id'
+  through: "ProductTag",
+  as: "tags",
+  foreignKey:'tag_id',
 })
 
 module.exports = {
