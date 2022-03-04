c149aae94b3b39dc45b94c080bf07cfdc907a90f
ramriddlez <ramankangz@outlook.com>
2022-03-03 19:20:34 -0500

    changes in routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,12 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',  async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll().then((catData) => {
-    res.json(catData);
-  });
+  try {
+  const catData = await Category.findAll({ 
+    include: [{ model: Product }],
+  })
+  res.status(200).json(catData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.get('/:id', (req, res) => {
