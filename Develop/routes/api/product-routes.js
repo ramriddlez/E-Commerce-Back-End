fc55228e479eee4496a4e85d63f36421d3561006
ramriddlez <ramankangz@outlook.com>
2022-03-03 20:45:09 -0500

    typo fix

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -19,7 +19,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
   });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try{
